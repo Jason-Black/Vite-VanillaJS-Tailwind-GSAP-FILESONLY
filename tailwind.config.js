aa82af6b8efa6483c524acac83240e429bccf7b3
Jason-Black <71369201+Jason-Black@users.noreply.github.com>
2024-08-24 20:18:49 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+      "web-files/index.html",
+      "./**/*.{js,ts,jsx,tsx,html,css"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
